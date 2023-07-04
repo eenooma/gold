2348069fc0c5f97409ca3e17c1f4cbeafc83924f
eenooma <eenooma@gmail.com>
2023-07-04 10:44:08 +0900

    Kboard 푸터 지우기

--- wp-content/plugins/kboard/skin/contact-form/admin-document.php
+++ wp-content/plugins/kboard/skin/contact-form/admin-document.php
@@ -126,7 +126,7 @@
 		
 		<?php if($board->contribution() && !$board->meta->always_view_list):?>
 		<div class="kboard-contact-form-poweredby">
-			<a href="https://www.cosmosfarm.com/products/kboard" onclick="window.open(this.href);return false;" title="<?php echo __('KBoard is the best community software available for WordPress', 'kboard')?>">Powered by KBoard</a>
+			<a href="https://www.cosmosfarm.com/products/kboard" onclick="window.open(this.href);return false;" title="<?php echo __('KBoard is the best community software available for WordPress', 'kboard')?>"></a>
 		</div>
 		<?php endif?>
 	</div>
